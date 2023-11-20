a9906e158ff9e9b9be0098917c63659ce38c0048
Gamejoongsa <jane10000@naver.com>
2023-11-20 14:43:14 +0900

    버튼 구현

--- lib/Login.dart
+++ lib/Login.dart
@@ -8,26 +8,41 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        appBar: MyAppBar(),
+    return Scaffold(
+        appBar: const MyAppBar(),
         body: Center(
-            child: TextButton(onPressed: kakaoLogin, child: Text("카카오 로그인"))
+            child: InkWell(
+              onTap: kakaoLogin,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('assets/images/kakao_login_large_wide.png',
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+            )
         )
     );
   }
-}
 
-void kakaoLogin() async {
-  if (await isKakaoTalkInstalled()) {
-    try {
-      await UserApi.instance.loginWithKakaoTalk();
-      print('카카오톡으로 로그인 성공');
-    } catch (error) {
-      print('카카오톡으로 로그인 실패 $error');
+  void kakaoLogin() async {
+    if (await isKakaoTalkInstalled()) {
+      try {
+        await UserApi.instance.loginWithKakaoTalk();
+        print('카카오톡으로 로그인 성공');
+      } catch (error) {
+        print('카카오톡으로 로그인 실패 $error');
 
-      if (error is PlatformException && error.code == 'CANCELED') {
-        return;
+        if (error is PlatformException && error.code == 'CANCELED') {
+          return;
+        }
+        try {
+          await UserApi.instance.loginWithKakaoAccount();
+          print('카카오계정으로 로그인 성공');
+        } catch (error) {
+          print('카카오계정으로 로그인 실패 $error');
+        }
       }
+    } else {
       try {
         await UserApi.instance.loginWithKakaoAccount();
         print('카카오계정으로 로그인 성공');
@@ -35,12 +50,6 @@ void kakaoLogin() async {
         print('카카오계정으로 로그인 실패 $error');
       }
     }
-  } else {
-    try {
-      await UserApi.instance.loginWithKakaoAccount();
-      print('카카오계정으로 로그인 성공');
-    } catch (error) {
-      print('카카오계정으로 로그인 실패 $error');
-    }
   }
 }
+
