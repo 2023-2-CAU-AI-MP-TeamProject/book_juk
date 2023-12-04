8408e7530f75de6b54d9731bfc244d1fc5a02ee2
JM <mulgyul203@cau.ac.kr>
2023-12-04 16:47:02 +0900

    JM20231116

--- lib/MyHome.dart
+++ lib/MyHome.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'MyAppBar.dart';
 
@@ -8,19 +10,98 @@ class MyHome extends StatefulWidget {
   State<MyHome> createState() => _MyHomeState();
 }
 class _MyHomeState extends State<MyHome> {
+  List<Widget> bookcaseList = [];
+  double _initialScale = 1.0;
+  double _scaleFactor = 1.0;
+  double _rotation = 1.0;
+
+  @override
+  void initState() {
+    super.initState();
+    for(int i = 0; i<5; i++) {
+      addBookCase();
+    }
+  }
+
+  void addBookCase() {
+    setState(() {
+      bookcaseList.add(BookShelf());
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: MyAppBar(),
-      body: Center(
-        child: Text('Home',
-          style: TextStyle(
-            fontSize: 100
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: 50,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.purpleAccent
+            ),
+            child: Text("여기는 검색", textAlign: TextAlign.center,),
           ),
-          textAlign: TextAlign.center,
-        )
-      )
+          Container(
+            height: 50,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.purple
+            ),
+            child: Text("읽은 책 / 읽고 싶은 책", textAlign: TextAlign.center,),
+          ),
+          Expanded(
+            child: GestureDetector(
+              onScaleStart: (details) {
+                setState(() {
+                  _initialScale = _scaleFactor;
+                });
+              },
+              onScaleUpdate: (details) {
+                setState(() {
+                  _scaleFactor = _initialScale * details.scale;
+                });
+              },
+                child: SingleChildScrollView(
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(50, 20, 50, 0),
+                    padding: EdgeInsets.only(bottom: 15),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Color(0xffC5965E),
+                      borderRadius: BorderRadius.circular(10)
+                    ),
+                    child: Column(
+                      children: bookcaseList
+                    ),
+                  ),
+                ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: addBookCase,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class BookShelf extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(15, 15, 15, 0),
+      height: 100,
+      width: double.infinity,
+      decoration: BoxDecoration(
+          color: Color(0xffC5965E),
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: Colors.black)
+      ),
+      child: Row(),
     );
   }
 }
