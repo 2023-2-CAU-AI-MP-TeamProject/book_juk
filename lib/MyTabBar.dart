cbf8148dce3a98e5b45ab8447510926a039e0222
Gamejoongsa <jane10000@naver.com>
2023-12-08 18:37:02 +0900

    Issue/#17 Resolved.

--- lib/MyTabBar.dart
+++ lib/MyTabBar.dart
@@ -3,42 +3,16 @@ import 'package:flutter/material.dart';
 // ignore: must_be_immutable
 class MyTabBar extends StatelessWidget {
   TabController tabController;
+  Function onTap;
 
   MyTabBar({
     super.key,
-    required this.tabController
+    required this.tabController,
+    required this.onTap
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.transparent,
-      child: TabBar(
-        tabs: const <Tab>[
-          Tab(
-            icon: Icon(Icons.home, size: 30),
-            //text: "홈 화면"
-          ),
-          Tab(
-            icon: Icon(Icons.search, size: 30),
-            //text: "검색"
-          ),
-          Tab(
-            icon: Icon(Icons.analytics, size: 30),
-            //text: "통계"
-          ),
-          Tab(
-            icon: Icon(Icons.settings, size: 30),
-            //text: "설정"
-          )
-        ],
-        labelColor: Colors.blue,
-        unselectedLabelColor: const Color.fromRGBO(20, 20, 20, 0.3),
-        isScrollable: false,
-        indicatorColor: Colors.transparent,
-        tabAlignment: TabAlignment.fill,
-        controller: tabController,
-      ),
-    );
+    return Placeholder();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,7 @@ import 'package:book_juk/MyHome.dart';
 import 'package:flutter/material.dart';
 import 'Search.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'KeepAliveScreen.dart';
-import 'MyTabBar.dart';
+import 'CustomNavigator.dart';
 import 'Statistics.dart';
 
 void main() {
@@ -51,8 +50,7 @@ class MyApp extends StatelessWidget {
 
 
 class Landing extends StatefulWidget {
-  Landing({super.key});
-  
+  const Landing({super.key});
 
   @override
   State<Landing> createState() => _LandingState();
@@ -60,24 +58,30 @@ class Landing extends StatefulWidget {
 
 class _LandingState extends State<Landing>
 with SingleTickerProviderStateMixin {
-  final GlobalKey<NavigatorState> _searchKey = GlobalKey<NavigatorState>();
-  //final _navigatorKeyList = List.generate(4, (index) => GlobalKey<NavigatorState>());
+
+  DateTime? currentBackPressTime;
+  int _selectedIndex = 0;
+  final _navigatorKeyList = List.generate(4, (index) => GlobalKey<NavigatorState>());
   late TabController tabController = TabController(
     length: 4,
     vsync: this,
     animationDuration: Duration.zero
   );
 
+  void showSnackBar() {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text("현재 index: $_selectedIndex\n '뒤로' 버튼 한번 더 눌러 종료"),
+        duration: Duration(seconds: 2),
+      )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final List<Widget> pages = [
       MyHome(),
-      Navigator(
-        key: _searchKey,
-        onGenerateRoute: (settings) {
-          return MaterialPageRoute(builder: (context) => Search());
-        }
-      ),
+      Search(),
       Statistics(),
       Text(
         "SEEEEEEEEEETTTTTTTTTTTTTTTTTINGSSSSSSSSS!!!!!!!",
@@ -90,7 +94,17 @@ with SingleTickerProviderStateMixin {
 
     return WillPopScope(
       onWillPop: () async {
-        return !await _searchKey.currentState!.maybePop();
+        if(await _navigatorKeyList[_selectedIndex].currentState!.maybePop()){
+          return Future.value(false);
+        } else {
+          DateTime now = DateTime.now();
+          if(currentBackPressTime == null || now.difference(currentBackPressTime!) > Duration(seconds: 2)){
+            currentBackPressTime = now;
+            showSnackBar();
+            return Future.value(false);
+          }
+          return Future.value(true);
+        }
       },
       child: Scaffold(
         body: TabBarView(
@@ -98,12 +112,46 @@ with SingleTickerProviderStateMixin {
           physics: NeverScrollableScrollPhysics(),
           children: pages.map(
             (page) {
-              //int index = _pages.indexOf(page);
-              return KeepAliveScreen(page: page);
+              int index = pages.indexOf(page);
+              return CustomNavigator(
+                page: page,
+                navigatorKey: _navigatorKeyList[index]
+              );
             },
           ).toList()
         ),
-        bottomNavigationBar: MyTabBar(tabController: tabController),
+        bottomNavigationBar: Container(
+          color: Colors.transparent,
+          child: TabBar(
+            tabs: const <Tab>[
+              Tab(
+                icon: Icon(Icons.home, size: 30),
+                //text: "홈 화면"
+              ),
+              Tab(
+                icon: Icon(Icons.search, size: 30),
+                //text: "검색"
+              ),
+              Tab(
+                icon: Icon(Icons.analytics, size: 30),
+                //text: "통계"
+              ),
+              Tab(
+                icon: Icon(Icons.settings, size: 30),
+                //text: "설정"
+              )
+            ],
+            labelColor: Colors.blue,
+            unselectedLabelColor: const Color.fromRGBO(20, 20, 20, 0.3),
+            isScrollable: false,
+            indicatorColor: Colors.transparent,
+            tabAlignment: TabAlignment.fill,
+            controller: tabController,
+            onTap: (value) => setState(() {
+              _selectedIndex = value;
+            }),
+          ),
+        )
       ),
     );
   }
