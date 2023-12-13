ff6f9115e68567df95ef591078970bf684fc1ac7
Gamejoongsa <jane10000@naver.com>
2023-12-13 15:50:17 +0900

    Popup 구현 2

--- lib/BookStoreDialog.dart
+++ lib/BookStoreDialog.dart
@@ -9,13 +9,12 @@ class BookStoreDialog extends StatefulWidget {
 
 class _BookStoreDialogState extends State<BookStoreDialog> {
   final _isSelected = <bool>[true, false];
-  late final double _buttonWidth;
-  late final double _buttonHeight;
+  DateTime date = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
-    _buttonWidth = MediaQuery.of(context).size.width / 2.3;
-    _buttonHeight = MediaQuery.of(context).size.width / 3.5;
+    final double buttonWidth = MediaQuery.of(context).size.width / 2.3;
+    final double buttonHeight = MediaQuery.of(context).size.width / 3.5;
 
     return Dialog(
       alignment: Alignment.bottomCenter,
@@ -38,58 +37,95 @@ class _BookStoreDialogState extends State<BookStoreDialog> {
               children: [
                 Padding(
                   padding: const EdgeInsets.all(10.0),
-                  child: InkWell(
-                    onTap: () {
-                      _isSelected[0] = true;
-                      _isSelected[1] = false;
-                    },
-                    splashColor: Colors.black,
-                    child: Container(
-                      width: _buttonWidth, height: _buttonHeight,
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                        color: Colors.lightBlue
-                      ),
+                  child: Ink(
+                    height: buttonHeight,
+                    width: buttonWidth,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: (_isSelected[0]) ? Colors.lightBlue : Colors.lightBlue.withOpacity(0.5),
+                    ),
+                    child: InkWell(
+                      onTap: () {
+                        _isSelected[0] = true;
+                        _isSelected[1] = false;
+                        setState(() {});
+                      },
+                      borderRadius: BorderRadius.circular(20),
+                      splashColor: Colors.blue,
                       child: Center(
                         child: Text('읽은 책',
                           style: TextStyle(
-                            color: Colors.black,
-                            fontSize: _buttonWidth / 6
+                            color: (_isSelected[0]) ? Colors.black : Colors.black.withOpacity(0.5),
+                            fontSize: buttonWidth / 6
                           ),
                         ),
                       )
-                    )
+                    ),
                   ),
                 ),
                 Padding(
                   padding: const EdgeInsets.all(10.0),
-                  child: InkWell(
-                    onTap: () {
-                      _isSelected[0] = false;
-                      _isSelected[1] = true;
-                    },
-                    child: Container(
-                      width: _buttonWidth, height: _buttonHeight,
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                        color: Colors.lightBlue
-                      ),
+                  child: Ink(
+                    height: buttonHeight,
+                    width: buttonWidth,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: (_isSelected[1]) ? Colors.lightBlue : Colors.lightBlue.withOpacity(0.5),
+                    ),
+                    child: InkWell(
+                      onTap: () {
+                        _isSelected[0] = false;
+                        _isSelected[1] = true;
+                        setState(() {});
+                      },
+                      borderRadius: BorderRadius.circular(20),
+                      splashColor: Colors.blue,
                       child: Center(
                         child: Text('읽고 싶은 책',
                           style: TextStyle(
-                            color: Colors.black,
-                            fontSize: _buttonWidth / 6
+                            color: (_isSelected[1]) ? Colors.black : Colors.black.withOpacity(0.5),
+                            fontSize: buttonWidth / 6
                           ),
                         ),
                       )
-                    )
+                    ),
                   ),
                 ),
               ],
             ),
             ElevatedButton(
-              onPressed: () {},
-              child: SizedBox(height: 30, width: MediaQuery.of(context).size.width - 50,)
+              onPressed: () async {
+                final selectedDate = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(2000),
+                  lastDate: DateTime(2077),
+                  initialEntryMode: DatePickerEntryMode.calendarOnly
+                );
+                setState(() {
+                  date = selectedDate ?? date;
+                });
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.transparent),
+                elevation: MaterialStateProperty.all(0),
+                overlayColor: MaterialStateProperty.all(Colors.grey)
+              ),
+              child: SizedBox(
+                height: 30,
+                width: MediaQuery.of(context).size.width - 50,
+                child: Center(
+                  child: LayoutBuilder(
+                    builder: (context, constraints) => Text(
+                      '날짜 : ${date.year} - ${date.month} - ${date.day}',
+                      style: TextStyle(
+                        fontSize: constraints.maxWidth / 20,
+                        color: Colors.black
+                      )
+                    ),
+                  ),
+                )
+              )
             )
           ],
         ),
