321cdd4cc017b21e588ea7d90e38f6aa5c6406e7
JM <mulgyul203@cau.ac.kr>
2023-12-16 15:56:39 +0900

    책장 확대 및 축소 방식 GestureDetector → InteractiveViewer 변경 책장 확대 및 축소 제한 설정 변경

--- lib/MyHome.dart
+++ lib/MyHome.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 
 class MyHome extends StatefulWidget {
@@ -10,11 +9,6 @@ class MyHome extends StatefulWidget {
 
 class _MyHomeState extends State<MyHome> {
   List<Widget> bookcaseList = [];
-  Matrix4 _transformationMatrix = Matrix4.identity();
-  double _scale = 1.0;
-  late Offset _previousOffset;
-  Offset _currentOffset = Offset.zero;
-  late double _previousScale;
 
   @override
   void initState() {
@@ -32,82 +26,56 @@ class _MyHomeState extends State<MyHome> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onScaleStart: (ScaleStartDetails details) {
-        _previousScale = _scale;
-        _previousOffset = details.focalPoint;
-      },
-      onScaleUpdate: (ScaleUpdateDetails details) {
-        setState(() {
-          double newScale = _previousScale * details.scale;
-          _scale = max(1.0, newScale);
-          _currentOffset = details.focalPoint;
-          _transformationMatrix = Matrix4.identity()
-            ..translate(_currentOffset.dx - _previousOffset.dx, _currentOffset.dy - _previousOffset.dy)
-            ..scale(_scale, _scale, 1);
-        });
-      },
-      onScaleEnd: (ScaleEndDetails details) {
-        if (_scale == 1.0) {
-          setState(() {
-            _transformationMatrix = Matrix4.identity();
-            _previousOffset = Offset.zero;
-          });
-        }
-        _previousScale = _scale;
-        _previousOffset = _currentOffset;
-      },
-      child: Transform(
-        transform: _transformationMatrix,
-        alignment: FractionalOffset.center,
-        child: Scaffold(
-          body: Column(
-            children: [
-              Container(
-                height: 50,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Colors.purpleAccent,
-                ),
-                child: Text(
-                  "여기는 검색",
-                  textAlign: TextAlign.center,
-                ),
+    return Scaffold(
+      body: InteractiveViewer(
+        minScale: 1.0,
+        maxScale: 2.0,
+        child: Column(
+          children: [
+            Container(
+              height: 50,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.purpleAccent,
               ),
-              Container(
-                height: 50,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Colors.purple,
-                ),
-                child: Text(
-                  "읽은 책 / 읽고 싶은 책",
-                  textAlign: TextAlign.center,
-                ),
+              child: Text(
+                "여기는 검색",
+                textAlign: TextAlign.center,
               ),
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Container(
-                    margin: EdgeInsets.fromLTRB(50, 20, 50, 0),
-                    padding: EdgeInsets.only(bottom: 15),
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Color(0xffC5965E),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Column(
-                      children: bookcaseList,
-                    ),
+            ),
+            Container(
+              height: 50,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.purple,
+              ),
+              child: Text(
+                "읽은 책 / 읽고 싶은 책",
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Container(
+                  margin: EdgeInsets.fromLTRB(50, 150, 50, 0),
+                  padding: EdgeInsets.only(bottom: 15),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Color(0xffC5965E),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    children: bookcaseList,
                   ),
                 ),
               ),
-            ],
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: addBookCase,
-            child: Icon(Icons.add),
-          ),
+            ),
+          ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: addBookCase,
+        child: Icon(Icons.add),
       ),
     );
   }
