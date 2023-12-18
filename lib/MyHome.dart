e2dbd74b7f9b8f72e10e8317818cf765e4683870
JM <mulgyul203@cau.ac.kr>
2023-12-18 22:26:59 +0900

    책장이 기기에서 뜨지 않는 오류 해결방안1. Expanded를 다시 Widget으로 감싼다.

--- lib/MyHome.dart
+++ lib/MyHome.dart
@@ -30,25 +30,29 @@ class _MyHomeState extends State<MyHome> {
       appBar: AppBar(
         title: Text("임의로 설정한 타이틀", style: TextStyle(color: Colors.purpleAccent)),
       ),
-      body: Expanded(
-        child: InteractiveViewer(
-          minScale: 1.0,
-          maxScale: 2.0,
-          child: SingleChildScrollView(
-            child: Container(
-              margin: EdgeInsets.fromLTRB(50, 20, 50, 0),
-              padding: EdgeInsets.only(bottom: 15),
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Color(0xffC5965E),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                children: bookcaseList,
+      body: Column(
+        children: [
+          Expanded(
+            child: InteractiveViewer(
+              minScale: 1.0,
+              maxScale: 2.0,
+              child: SingleChildScrollView(
+                child: Container(
+                  margin: EdgeInsets.fromLTRB(50, 20, 50, 0),
+                  padding: EdgeInsets.only(bottom: 15),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Color(0xffC5965E),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    children: bookcaseList,
+                  ),
+                ),
               ),
             ),
           ),
-        ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: addBookCase,
